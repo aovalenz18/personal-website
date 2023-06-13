03f81d88745fd5fca0b1d4e6c00eadfc054d2b9e
Anthony Omar Valenzuela <aovalenz@uci.edu>
2023-06-12 22:07:18 -0700

    creating a nav bar

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -1,9 +1,39 @@
 'use client'
+import Head from "next/head";
 import { styled } from "styled-components";
 
-const NavBar = styled.div`
-    display: flex;
-    background: red;
+
+const HeadingName = styled.h3`
+    color: #ea744c;
+    padding-top: 0.5rem;
 `;
+
+const StyledButton = styled.button`
+    color: #c5ca54;
+    background: transparent;
+    border: none;
+`;
+
+const NavBarContainer = styled.div`
+    display: flex;
+    background-color: #015265;
+    border-top: 2px solid #d4cac9;
+    justify-content: space-evenly;
+`;
+
+const NavBar = (props) => {
+
+    const sections = ["Home", "Projects", "Personal Life"];
+
+    return (
+    <NavBarContainer {...props}>
+    <HeadingName>Anthony Omar Valenzuela</HeadingName>
+    {sections.map((section, index) => {
+        return <StyledButton key={index}>{section}</StyledButton>
+    })}
+    </NavBarContainer>
+    
+    )
+};
 
 export default NavBar;
--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,7 @@ const sections = ["Home, Projects, Personal Life"]
 export default function Home() {
   return (
     <>
-    <div> Hello, welcome to my site !</div>
-    {sections.map((sections, i)=> (
-      <div key={i}>{sections}</div>
-    ))}
+    <NavBar></NavBar>
     </>
   )
 }
